19c3011e4525d857724e41d64081b50b0cbd2bab
Georges Gressot <gressot@unistra.fr>
2020-01-07 13:54:20 +0100

    replymail admin added and doc updated

--- eplouribousse/apps/epl/admin.py
+++ eplouribousse/apps/epl/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Library, ItemRecord, Instruction, Feature, Exclusion, BddAdmin
+from .models import Project, Library, ItemRecord, Instruction, Feature, Exclusion, BddAdmin, ReplyMail
 
 admin.site.register(Project)
 admin.site.register(Library)
@@ -10,3 +10,4 @@ admin.site.register(Instruction)
 admin.site.register(Feature)
 admin.site.register(Exclusion)
 admin.site.register(BddAdmin)
+admin.site.register(ReplyMail)
