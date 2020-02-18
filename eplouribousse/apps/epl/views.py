bc9f3e3a9615144cb4a048e59aa779b15736a69b
Georges Gressot <gressot@unistra.fr>
2020-02-18 08:59:18 +0100

    x1arb view : DRY correction

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -700,8 +700,7 @@ def x1arb(request, lid, xlid):
 
     for e in ItemRecord.objects.filter(lid =lid, rank = 1, status =0):
         sid = e.sid
-        if ItemRecord.objects.exclude(lid =lid).filter(sid =sid, rank = 1) and \
-        ItemRecord.objects.filter(sid =sid, lid = xlid, rank = 1):
+        if ItemRecord.objects.filter(sid =sid, lid = xlid, rank = 1):
             resslist.append(e)
 
     size = len(resslist)
