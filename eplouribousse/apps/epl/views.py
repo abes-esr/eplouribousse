f1606b0d4456bfd60f2a5c27c00f90eca7a88b0c
Georges Gressot <gressot@unistra.fr>
2020-05-12 10:08:43 +0200

    Chrodechilde mise au niveau d'Ultrogothe (1.x.2)

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -1,8 +1,8 @@
-# epl_version ="Version 1.2.1 (Chrodechilde)"
-# date_version ="May 11, 2020"
+epl_version ="Version 1.2.2 (Chrodechilde)"
+date_version ="May 12, 2020"
 # Mise au niveau de :
-epl_version ="Version 1.3.1 beta (~Ultrogothe)"
-date_version ="May 11, 2020"
+# epl_version ="Version 1.3.2 beta (~Ultrogothe)"
+# date_version ="May 12, 2020"
 
 
 from django.shortcuts import render
