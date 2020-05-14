c34c9e32bcbb39553016e0da4b533500979f8f2a
Georges Gressot <gressot@unistra.fr>
2020-05-14 10:29:11 +0200

    Ajout de textes d'aide à la saisie dans les formulaires

--- eplouribousse/apps/epl/forms.py
+++ eplouribousse/apps/epl/forms.py
@@ -9,6 +9,7 @@ class PositionForm(forms.ModelForm):
         model = ItemRecord
         fields = ('rank', 'excl', 'comm',)
         widgets = {
+            'rank' : forms.Select(attrs={'title': _("Choisissez 1 pour la collection mère ; 2, 3 ou 4 selon l'importance de votre collection ou d'autres raisons ...")}),
             'excl' : forms.Select(choices=EXCLUSION_CHOICES),
             'comm' : forms.Textarea(attrs={'placeholder': _("Commentaire éventuel pour expliquer votre choix (max. 250 caractères)")}),
         }
@@ -21,9 +22,11 @@ class InstructionForm(forms.ModelForm):
         widgets = {
             'line' : forms.TextInput(attrs={'title': _("Respectez l'ordre chronologique du champ 'Segment'")}),
             'oname' : forms.TextInput(attrs={'title': _("Intitulé de la bibliothèque ayant précédemment déclaré une 'exception' ou un 'améliorable'")}),
-            'descr' : forms.TextInput(attrs={'placeholder': _("1990(2)-1998(12) par ex.")}),
-            'exc' : forms.TextInput(attrs={'placeholder': _("1991(5) par ex.")}),
-            'degr' : forms.TextInput(attrs={'placeholder': _("1995(4) par ex.")}),
+            'descr' : forms.TextInput(attrs={'placeholder': _("1990(2)-1998(12) par ex."), 'title': _("Suite ininterrompue chronologiquement")}),
+            'exc' : forms.TextInput(attrs={'placeholder': _("1991(5) par ex."), 'title': \
+            _("éléments manquants dans le segment pour la forme considérée (pas forcément des lacunes si l'on considère la forme reliée)")}),
+            'degr' : forms.TextInput(attrs={'placeholder': _("1995(4) par ex."), 'title': \
+            _("éléments dégradé (un volume relié dégradé peut être remplacé par les fascicules correspondants en bon état)")}),
         }
 
 
