9ab4f7d4a4d9546f65caddb9c525c516964e61aa
Georges Gressot <gressot@unistra.fr>
2020-05-15 17:23:27 +0200

    Ultrogothe 1.3.13

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -1,8 +1,8 @@
-epl_version ="Version 1.2.12 (Chrodechilde)"
-date_version ="May 14, 2020"
+epl_version ="Version 1.2.13 (Chrodechilde)"
+date_version ="May 15, 2020"
 # Mise au niveau de :
-# epl_version ="Version 1.3.12 beta (~Ultrogothe)"
-# date_version ="May 14, 2020"
+epl_version ="Version 1.3.13 beta (~Ultrogothe)"
+date_version ="May 15, 2020"
 
 
 from django.shortcuts import render
