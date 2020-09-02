fc4ce03eaa4c5a9ded3aefe5dbc02f981033ec99
Georges Gressot <gressot@unistra.fr>
2020-09-02 16:14:48 +0200

    1._8~9.0

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="Version 1.8.0 (Radegonde)"
 date_version ="September 2, 2020"
 # Mise au niveau de :
-# epl_version ="Version 1.9.0 beta (~Walderade)"
-# date_version ="September 2, 2020"
+epl_version ="Version 1.9.0 beta (~Walderade)"
+date_version ="September 2, 2020"
 
 
 from django.shortcuts import render
