0e41f328c45a3efcf838838f0feb757ef813da97
Georges Gressot <gressot@unistra.fr>
2020-09-30 08:26:17 +0200

    v.1.11-beta.4 starts

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v1.10.3 (Ingeberge)"
 date_version ="September 24, 2020"
 # Mise au niveau de :
-# epl_version ="v1.11-beta.3 (~Merofledis)"
-# date_version ="September 30, 2020"
+epl_version ="v1.11-beta.3 (~Merofledis)"
+date_version ="September 30, 2020"
 
 
 from django.shortcuts import render
