22fdbca1a83d8ad9379d33a3c0cd1774cce3ab5c
Georges Gressot <gressot@unistra.fr>
2020-10-20 08:19:27 +0200

    v1.13-beta.3 (~Galswinthe) starts

--- eplouribousse/apps/epl/views.py
+++ eplouribousse/apps/epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v1.12.2 (Audovera)"
 date_version ="October 14, 2020"
 # Mise au niveau de :
-# epl_version ="v1.13-beta.2 (~Galswinthe)"
-# date_version ="October 20, 2020"
+epl_version ="v1.13-beta.2 (~Galswinthe)"
+date_version ="October 20, 2020"
 
 
 from django.shortcuts import render
