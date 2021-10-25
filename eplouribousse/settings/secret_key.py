0c9b33a0d70353db1f7faa7ad6795261c4763414
GGre <gressot@unistra.fr>
2021-10-25 14:01:28 +0200

    jsfmh

--- eplouribousse/settings/secret_key.py
+++
@@ -1,7 +0,0 @@
-"""
-Ce fichier renferme la clef secrète (mise ici et exclue du dépôt dans .gitignore afin qu'elle ne soit pas resquillée)
-"""
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()1bdh=2vyryvvuzh%kp!ovheq2)495vql^l&lc@$(h78#00wj'
