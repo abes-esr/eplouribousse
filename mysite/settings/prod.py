01598efc3f6439b79001162f682ee095875732bf
GGre <gressot@unistra.fr>
2022-10-20 15:48:30 +0200

    on va voir

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -6,3 +6,8 @@ DEBUG = False
 
 # Messages backend (sortie des messages):
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'eplouribousse@gmail.com'
+from .secret_key import EMAIL_HOST_PASSWORD
