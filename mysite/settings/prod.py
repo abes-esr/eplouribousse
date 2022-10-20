f6688a05b19cc7f3af93c2165246d93792d38f2d
GGre <gressot@unistra.fr>
2022-10-20 17:04:15 +0200

    return to safe

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -5,9 +5,4 @@ from .base import *
 DEBUG = False
 
 # Messages backend (sortie des messages):
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eplouribousse@gmail.com'
-from .secret_key import EMAIL_HOST_PASSWORD
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
