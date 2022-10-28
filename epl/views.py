6976a4c075a5df9e1c81de489d09d2c36fa203d3
GGre <gressot@unistra.fr>
2022-10-28 12:24:11 +0200

    correction CGU

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.08.8 (Fastrada)"
 date_version ="October 28, 2022"
 # Mise au niveau de :
-epl_version ="v2.09.8 (~Luitgard)"
-date_version ="October 28, 2022"
+#epl_version ="v2.09.8 (~Luitgard)"
+#date_version ="October 28, 2022"
 
 
 from django.shortcuts import render, redirect
