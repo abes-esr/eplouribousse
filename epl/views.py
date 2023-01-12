5a92cca1a7fa2452c9055635513aef63bc88e698
GGre <gressot@unistra.fr>
2023-01-12 12:07:36 +0100

    synchronisations bdd projets >< principale

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.10.1 (Judith)"
 date_version ="January 12, 2023"
 # Mise au niveau de :
-#epl_version ="v2.11.1 (~Irmingard)"
-#date_version ="January 12, 2023"
+epl_version ="v2.11.1 (~Irmingard)"
+date_version ="January 12, 2023"
 
 
 from django.shortcuts import render, redirect
