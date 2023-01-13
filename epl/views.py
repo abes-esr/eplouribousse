22a3535e001445fbc3f60a15895ed17bf461a337
GGre <gressot@unistra.fr>
2023-01-13 12:55:10 +0100

    mail suppression de compte

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.10.2 (Judith)"
 date_version ="January 12, 2023"
 # Mise au niveau de :
-#epl_version ="v2.11.2 (~Irmingard)"
-#date_version ="January 12, 2023"
+epl_version ="v2.11.2 (~Irmingard)"
+date_version ="January 12, 2023"
 
 
 from django.shortcuts import render, redirect
