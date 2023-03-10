4ecfe9385da8dd1acff295837f21fac5a3ab318c
GGre <gressot@unistra.fr>
2023-03-10 12:00:15 +0100

    Send to main page in case of unconsistent project

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.10.8 (Judith)"
 date_version ="March 10, 2023"
 # Mise au niveau de :
-epl_version ="v2.11.8 (~Irmingard)"
-date_version ="March 10, 2023"
+#epl_version ="v2.11.8 (~Irmingard)"
+#date_version ="March 10, 2023"
 
 
 from django.shortcuts import render, redirect
