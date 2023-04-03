26a2d06f83ca91ecb0e62e1003d49397c4ba3ceb
GGre <gressot@unistra.fr>
2023-04-03 08:56:51 +0200

    emails in lower case and timezone ...

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -123,13 +123,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-fr'
 
+USE_TZ = True
+
 TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_L10N = True
-
-USE_TZ = True
 
 gettext = lambda x: x
 
