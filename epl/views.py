a0310607e4432e2ea3b2467c84af173f388eeb9f
GGre <gressot@unistra.fr>
2023-05-26 13:06:03 +0200

    suite résulotion ticket #38 bckp

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
-epl_version ="v2.10.56 (Judith)"
+epl_version ="v2.10.57 (Judith)"
 date_version ="May 26, 2023"
 # Mise au niveau de :
-epl_version ="v2.11.56 (~Irmingard)"
-date_version ="May 26, 2023"
+#epl_version ="v2.11.57 (~Irmingard)"
+#date_version ="May 26, 2023"
 
 
 from django.shortcuts import render, redirect
