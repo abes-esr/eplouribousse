d822a7d30e7e4ea32253cc096596b03958ef4a30
GGre <gressot@unistra.fr>
2023-05-31 10:34:02 +0200

    rtdamiim

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.10.59 (Judith)"
 date_version ="May 31, 2023"
 # Mise au niveau de :
-#epl_version ="v2.11.59 (~Irmingard)"
-#date_version ="May 31, 2023"
+epl_version ="v2.11.59 (~Irmingard)"
+date_version ="May 31, 2023"
 
 
 from django.shortcuts import render, redirect
