7fe4f35f3f516b1b644c5c649d51b4cdd8b15c3e
GGre <gressot@unistra.fr>
2023-06-08 14:50:05 +0200

    màj manuel

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
-epl_version ="v2.10.67 (Judith)"
+epl_version ="v2.10.68 (Judith)"
 date_version ="June 8, 2023"
 # Mise au niveau de :
-epl_version ="v2.11.67 (~Irmingard)"
-date_version ="June 8, 2023"
+#epl_version ="v2.11.68 (~Irmingard)"
+#date_version ="June 8, 2023"
 
 
 from django.shortcuts import render, redirect
