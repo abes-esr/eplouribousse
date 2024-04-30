42b707b723ae48f0bd1b54bd2ce6edbdce3ce505
GGre <gressot@unistra.fr>
2024-04-30 16:35:57 +0200

    rtdammim after ticket#52

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.11.112 (Judith)"
 date_version ="April 30, 2024"
 # Mise au niveau de :
-#epl_version ="v2.11.113 (~Irmingard)"
-#date_version ="April 30, 2024"
+epl_version ="v2.11.113 (~Irmingard)"
+date_version ="April 30, 2024"
 
 
 from django.shortcuts import render, redirect
