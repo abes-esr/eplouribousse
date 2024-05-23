ae9d7e2c4e6fdfecd8936b4a8d278810b52b42bc
GGre <gressot@unistra.fr>
2024-05-23 08:46:33 +0200

    rtdamiim after Ticket #55 ordre des lignes

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.11.128 (Judith)"
 date_version ="May 23, 2024"
 # Mise au niveau de :
-#epl_version ="v2.11.129 (~Irmingard)"
-#date_version ="May 23, 2024"
+epl_version ="v2.11.129 (~Irmingard)"
+date_version ="May 23, 2024"
 
 
 from django.shortcuts import render, redirect
