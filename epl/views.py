d064d479379e154f89f8e42109d44e9ac7d0ded7
GGre <gressot@unistra.fr>
2024-06-03 09:29:56 +0200

    rtdamiim < collections au lieu de rattachements

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.11.142 (Judith)"
 date_version ="June 03, 2024"
 # Mise au niveau de :
-#epl_version ="v2.11.143 (~Irmingard)"
-#date_version ="June 03, 2024"
+epl_version ="v2.11.143 (~Irmingard)"
+date_version ="June 03, 2024"
 
 from django.shortcuts import render, redirect
 
