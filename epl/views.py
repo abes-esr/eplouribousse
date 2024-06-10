d7ca9ae0219bbcfc3f43410e7945dc17c6c1ed24
GGre <gressot@unistra.fr>
2024-06-10 19:14:26 +0200

    rtdamiim < anonymous geht nix

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.11.150 (Judith)"
 date_version ="June 11, 2024"
 # Mise au niveau de :
-#epl_version ="v2.11.151 (~Irmingard)"
-#date_version ="June 11, 2024"
+epl_version ="v2.11.151 (~Irmingard)"
+date_version ="June 11, 2024"
 
 from django.shortcuts import render, redirect
 
