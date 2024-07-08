424dc6787e88418297d641426d1e4a72871333ac
GGre <gressot@unistra.fr>
2024-07-08 14:56:03 +0200

    rtdamiim < def search cancelled and -attribuer le tour-

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
 epl_version ="v2.11.160 (Judith)"
 date_version ="July 08, 2024"
 # Mise au niveau de :
-#epl_version ="v2.11.161 (~Irmingard)"
-#date_version ="July 08, 2024"
+epl_version ="v2.11.161 (~Irmingard)"
+date_version ="July 08, 2024"
 
 from django.shortcuts import render, redirect
 
