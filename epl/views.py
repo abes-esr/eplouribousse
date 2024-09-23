fae3ca0e9e39d4261cc4f655d4734396c1fbcf0b
GGre <gressot@unistra.fr>
2024-09-23 10:11:10 +0200

    Tickets 70 et 71

--- epl/views.py
+++ epl/views.py
@@ -1,8 +1,8 @@
-epl_version ="v2.11.164 (Judith)"
-date_version ="September 19, 2024"
+epl_version ="v2.11.166 (Judith)"
+date_version ="September 23, 2024"
 # Mise au niveau de :
-epl_version ="v2.11.165 (~Irmingard)"
-date_version ="September 19, 2024"
+#epl_version ="v2.11.167 (~Irmingard)"
+#date_version ="September 23, 2024"
 
 from django.shortcuts import render, redirect
 
@@ -2279,11 +2279,21 @@ def indicators(request, bdd):
             y7.append(np.nan)
         
     uri7 =get_scatter(x7, y7, _(""), _(""), _("Positionnements réalisés en % des rattachements"))
-    
+
+########################################
+#20/09/2024
+    last_sid, last =[],[]
     if len(Instruction.objects.using(bdd).all()) >9:
         last_instr =Instruction.objects.using(bdd).all().order_by('-pk')[0:10]
     else:
         last_instr =Instruction.objects.using(bdd).all().order_by('-pk')[0:len(Instruction.objects.using(bdd).all())]
+    for inst in last_instr:
+        if inst.sid not in last_sid:
+            last_sid.append(inst.sid)
+            last.append(((ItemRecord.objects.using(bdd).get(rank =1, sid =inst.sid)),inst))
+    lg =len(last)
+    lh =len(last_instr)
+########################################
 
     libch = ('',''),
     if Library.objects.using(bdd).all().exclude(lid ="999999999"):
@@ -2526,9 +2536,9 @@ def indicators_x(request, bdd, lid):
     for libmt in qs:
         x7.append(libmt.name)
 
-        pos =len([item for item in list(ItemRecord.objects.using(bdd).filter(lid=libmt.lid).exclude(rank =99)) if len(ItemRecord.objects.using(bdd).filter(lid =lid, sid =item.sid).exclude(rank =0))])
+        pos =len([item for item in list(ItemRecord.objects.using(bdd).filter(lid=libmt.lid).exclude(rank =99)) if len(ItemRecord.objects.using(bdd).filter(lid =lid, sid =item.sid).exclude(rank =0)) and len(ItemRecord.objects.using(bdd).filter(sid =item.sid).exclude(rank =0)) >1])
         
-        nopos =len([item for item in list(ItemRecord.objects.using(bdd).filter(lid =libmt.lid, rank =99)) if len(ItemRecord.objects.using(bdd).filter(lid =lid, sid =item.sid).exclude(rank =0))])
+        nopos =len([item for item in list(ItemRecord.objects.using(bdd).filter(lid =libmt.lid, rank =99)) if len(ItemRecord.objects.using(bdd).filter(lid =lid, sid =item.sid).exclude(rank =0)) and len(ItemRecord.objects.using(bdd).filter(sid =item.sid).exclude(rank =0)) >1])
         
         nopostotal +=nopos
         
@@ -2540,19 +2550,20 @@ def indicators_x(request, bdd, lid):
             y7.append(np.nan)
     uri7 =get_scatter(x7, y7, _(""), _(""), _("Positionnements réalisés en % des rattachements (*)"))
     
-    last_instr_list =[]
-    compteur1, compteur2 =0,0
-    while compteur1 <10:
-        try:
-            if ItemRecord.objects.using(bdd).get(lid =lid, sid=Instruction.objects.using(bdd).all().order_by('-pk')[compteur2].sid) and len(ItemRecord.objects.using(bdd).filter(rank =0, lid =lid, sid=Instruction.objects.using(bdd).all().order_by('-pk')[compteur2].sid)) ==0:
-                last_instr_list.append(Instruction.objects.using(bdd).all().order_by('-pk')[compteur2])
-                compteur1 +=1
-                compteur2 +=1
-        except:
-            if compteur2 ==len(Instruction.objects.using(bdd).all()) -1:#Arrivé à ce point, il n'y a plus d'autres instructions à examiner ; on doit s'arrêter là.
-                return render(request, 'epl/indicators_x.html', locals())
-            else:
-                compteur2 +=1
+########################################
+#20/09/2024
+    last_sid, last =[],[]
+    if len(Instruction.objects.using(bdd).all()) >9:
+        last_instr =Instruction.objects.using(bdd).all().order_by('-pk')[0:10]
+    else:
+        last_instr =Instruction.objects.using(bdd).all().order_by('-pk')[0:len(Instruction.objects.using(bdd).all())]
+    for inst in last_instr:
+        if inst.sid not in last_sid and len(ItemRecord.objects.using(bdd).filter(lid =lid, sid=inst.sid)) and not len(ItemRecord.objects.using(bdd).filter(rank =0, lid =lid, sid=inst.sid)):
+            last_sid.append(inst.sid)
+            last.append(((ItemRecord.objects.using(bdd).get(rank =1, sid =inst.sid)),inst))
+    lg =len(last)
+    lh =len(last_instr)
+########################################
 
     return render(request, 'epl/indicators_x.html', locals())
 
